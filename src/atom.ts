a6a721a878713c237872ac3ad3870d091858896e
mamonde456 <mamonde456@gmail.com>
2023-01-06 19:36:02 +0900

    add: todoId 전역 변수, changeTodo 전역 변수

--- src/atom.ts
+++ src/atom.ts
@@ -4,3 +4,13 @@ export const isLogged = atom<boolean>({
   key: "isLogged",
   default: false,
 });
+
+export const todoId = atom<string | undefined>({
+  key: "todoId",
+  default: "",
+});
+
+export const isChange = atom<boolean>({
+  key: "isChange",
+  default: false,
+});
