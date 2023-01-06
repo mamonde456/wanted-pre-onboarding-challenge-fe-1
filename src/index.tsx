592d6155599998b9e120d761b853476d3ee30a1c
mamonde456 <mamonde456@gmail.com>
2023-01-06 19:36:46 +0900

    add: react query

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { RouterProvider } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import router from "./Router";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <RecoilRoot>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </RecoilRoot>
 );
