4c1fac9a6e03919e9e6b77f2c3925794efbe436e
mamonde456 <mamonde456@gmail.com>
2023-01-06 19:36:14 +0900

    add: todo api

--- src/api.ts
+++ src/api.ts
@@ -5,11 +5,10 @@ export interface IUser {
 
 export const userAPi = async ({ email, password }: IUser, login?: string) => {
   const response = await fetch(
-    `http://localhost:8080/users/${login ? "login" : "create"}`,
+    `${process.env.REACT_APP_API_URL}/users/${login ? "login" : "create"}`,
     {
       method: "post",
       headers: {
-        "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
@@ -18,6 +17,86 @@ export const userAPi = async ({ email, password }: IUser, login?: string) => {
       }),
     }
   );
-  const data = await response.json();
+  return await response.json();
+};
+
+export interface ITodo {
+  title: string;
+  content: string;
+}
+
+export const createTodoApi = async (
+  { title, content }: ITodo,
+  token: string
+) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/todos`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      title,
+      content,
+    }),
+  });
+  return await response.json();
+};
+
+export const getTodosApi = async (token: string) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/todos`, {
+    headers: {
+      Authorization: token,
+    },
+  });
+  const { data } = await response.json();
+
   return data;
+};
+
+export const getTodoByIdApi = async (token: string, id: string) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/todos/${id}`, {
+    headers: {
+      Authorization: token,
+    },
+  });
+  const { data } = await response.json();
+  return data;
+};
+
+export interface INewTodo extends ITodo {
+  id: string;
+  token: string;
+}
+
+export const updateTodoApi = async ({
+  title,
+  content,
+  token,
+  id,
+}: INewTodo) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/todos/${id}`, {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify({ title, content }),
+  });
+  return await response.json();
+};
+
+export interface IDelete {
+  token: string;
+  id: string;
+}
+
+export const deleteTodoApi = async ({ token, id }: IDelete) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/todos/${id}`, {
+    method: "delete",
+    headers: {
+      Authorization: token,
+    },
+  });
+  return await response.json();
 };
