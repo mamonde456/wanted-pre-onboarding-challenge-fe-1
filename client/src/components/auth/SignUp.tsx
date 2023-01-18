5e1f16a95d4154b09d829cc2544b0d6b43d8c015
mamonde456 <mamonde456@gmail.com>
2023-01-18 18:20:26 +0900

    update:파일 경로 변경

---
+++ client/src/components/auth/SignUp.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import useAuth from "../../hook/auth/useAuth";
+import { IAuth } from "../../types/auth";
+import { noticeMsgAtom, userAtom } from "../../atom";
+import SignUpView from "./views/SignUpView";
+
+export default function SignUp() {
+  const [user, setUser] = useRecoilState(userAtom);
+  const setNoticeMsg = useSetRecoilState(noticeMsgAtom);
+  const auth = useAuth();
+  const signUpProps: IAuth = {
+    onSubmit: (e) => {
+      e.preventDefault();
+      auth.signUp(user);
+    },
+    onChange: (e) => {
+      const {
+        currentTarget: { name, value },
+      } = e;
+      const { password, confirmPassword } = user;
+      if (password !== confirmPassword)
+        return setNoticeMsg("비밀번호가 같지 않습니다.");
+
+      setUser({ ...user, [name]: value });
+    },
+    user,
+  };
+
+  return <SignUpView {...signUpProps} />;
+}
