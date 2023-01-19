738bd00f257d08ba3095a17d2153174bf448585c
mamonde456 <mamonde456@gmail.com>
2023-01-19 21:26:16 +0900

    update: input value를 작성하지 않고 submit했을 때 등의 유효성 로직

--- client/src/components/toDo/ChangeTodos.tsx
+++ client/src/components/toDo/ChangeTodos.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { isChoosen, isModalOpenAtom, noticeMsgAtom, todoId } from "../../atom";
-import { IChangeTodoProps, ITodo } from "../../types/todo";
+import { IChangeTodoProps, ITodo } from "../../types/toDo/todo";
 import useDelete from "../../hook/mutation/useDelete";
 import useUpdate from "../../hook/mutation/useUpdate";
 import useGetTodoById from "../../hook/query/useGetTodoById";
@@ -20,6 +20,8 @@ export default function ChangeTodos() {
   const params = useParams();
   const deleteTodo = useDelete();
   const updateTodo = useUpdate();
+  const titleRef = useRef<HTMLInputElement>(null);
+  const contentRef = useRef<HTMLInputElement>(null);
   const [newToDo, setNewToDo] = useState<ITodo>({
     title: "",
     content: "",
@@ -47,12 +49,23 @@ export default function ChangeTodos() {
       e.preventDefault();
       if (!isDisabled) return;
       if (newToDo.title === "" && newToDo.content === "")
-        return setNoticeMsg("값이 변경되지 않았습니다.");
-      if (newToDo.title === "")
-        setNewToDo({ ...newToDo, title: toDo?.title || "" });
-      if (newToDo.content === "")
-        setNewToDo({ ...newToDo, content: toDo?.content || "" });
-      setNewToDo({ title: "", content: "" });
+        return setNoticeMsg("빈 값은 할 일로 설정할 수 없습니다.");
+      if (titleRef) {
+        const titleValue = titleRef?.current?.dataset.set;
+        if (newToDo.title === titleValue)
+          return setNoticeMsg("값이 변경되지 않았습니다.");
+        if (newToDo.title === "") {
+          setNewToDo({ ...newToDo, title: titleValue || "" });
+        }
+      }
+      if (contentRef) {
+        const contentValue = contentRef?.current?.dataset.set;
+        if (newToDo.title === contentValue)
+          return setNoticeMsg("값이 변경되지 않았습니다.");
+        if (newToDo.content === "") {
+          setNewToDo({ ...newToDo, content: contentValue || "" });
+        }
+      }
       setActionType("update");
       setIsOpen(true);
     },
@@ -63,7 +76,7 @@ export default function ChangeTodos() {
 
       setNewToDo({
         ...newToDo,
-        [name.trim()]: value.trim(),
+        [name]: value.trim(),
       });
     },
     onDelete() {
@@ -76,6 +89,8 @@ export default function ChangeTodos() {
     actionType,
     isLoading,
     toDo,
+    titleRef,
+    contentRef,
   };
 
   return <ChangeTodoView {...ChangeToDoProps} />;
