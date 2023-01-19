2b8bc699358a158b2970ae40f8b963afc20ce4d9
mamonde456 <mamonde456@gmail.com>
2023-01-19 21:26:45 +0900

    update: 컴포넌트 분리

--- client/src/components/common/Notice.tsx
+++ client/src/components/common/Notice.tsx
@@ -1,19 +1,9 @@
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
-import styled from "styled-components";
-import { noticeMsgAtom } from "../../atom";
 
-const Notification = styled.p`
-  width: 100vw;
-  height: 30px;
-  background-color: black;
-  text-align: center;
-  line-height: 30px;
-  color: white;
-  position: absolute;
-  bottom: -30px;
-  left: 0;
-`;
+import { noticeMsgAtom } from "../../atom";
+import { IProps } from "../../types/common/notice";
+import NoticeView from "./view/NoticeView";
 
 export default function Notice() {
   const [noticeMsg, setNoticeMsg] = useRecoilState(noticeMsgAtom);
@@ -21,7 +11,12 @@ export default function Notice() {
     const timer = setTimeout(() => {
       setNoticeMsg(null);
     }, 2000);
+
     return () => clearTimeout(timer);
   }, [noticeMsg]);
-  return <>{noticeMsg && <Notification>{noticeMsg}</Notification>}</>;
+
+  const props: IProps = {
+    noticeMsg,
+  };
+  return <NoticeView {...props} />;
 }
