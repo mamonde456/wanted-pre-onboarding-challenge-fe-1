e488c37b52285632d8aa0ece09d9833f96d9b874
mamonde456 <mamonde456@gmail.com>
2023-01-19 21:28:39 +0900

    add: 비밀번호 유효성 검사 추가

--- client/src/components/auth/SignUp.tsx
+++ client/src/components/auth/SignUp.tsx
@@ -1,30 +1,46 @@
-import React from "react";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import useAuth from "../../hook/auth/useAuth";
-import { IAuth } from "../../types/auth";
-import { noticeMsgAtom, userAtom } from "../../atom";
+import { IAuth } from "../../types/auth/auth";
+import { userAtom } from "../../atom";
 import SignUpView from "./views/SignUpView";
+import { useRef, useState } from "react";
 
 export default function SignUp() {
   const [user, setUser] = useRecoilState(userAtom);
-  const setNoticeMsg = useSetRecoilState(noticeMsgAtom);
+  const [passwordNotice, setPasswordNotice] = useState<string | null>(null);
+  const [confirmNotice, setConfirmNotice] = useState<string | null>(null);
   const auth = useAuth();
+  const signUpRef = useRef<HTMLFormElement>(null);
   const signUpProps: IAuth = {
     onSubmit: (e) => {
       e.preventDefault();
       auth.signUp(user);
+      if (!signUpRef) return;
+      signUpRef?.current?.reset();
     },
     onChange: (e) => {
       const {
         currentTarget: { name, value },
       } = e;
-      const { password, confirmPassword } = user;
-      if (password !== confirmPassword)
-        return setNoticeMsg("비밀번호가 같지 않습니다.");
+      const password = name === "password" && value;
+      const confirmPassword = name === "confirmPassword" && value;
 
+      if (password && password.length < 8) {
+        setPasswordNotice("비밀번호는 8자리 이상 작성해주세요.");
+      } else {
+        setPasswordNotice(null);
+      }
+      if (confirmPassword && password !== confirmPassword) {
+        setConfirmNotice("비밀번호가 같지 않습니다.");
+      } else {
+        setConfirmNotice(null);
+      }
       setUser({ ...user, [name]: value });
     },
     user,
+    passwordNotice,
+    confirmNotice,
+    signUpRef,
   };
 
   return <SignUpView {...signUpProps} />;
