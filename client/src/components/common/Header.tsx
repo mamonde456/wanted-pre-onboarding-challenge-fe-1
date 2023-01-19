94660d63e90422a179acc9702ddfe6ff1a9c06df
mamonde456 <mamonde456@gmail.com>
2023-01-19 21:27:19 +0900

    update: 컴포넌트 분리

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,22 +1,7 @@
-import { Link } from "react-router-dom";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import styled from "styled-components";
 import { isLogged, noticeMsgAtom, todoId } from "../../atom";
-import Notice from "./Notice";
-
-const Wrapper = styled.header`
-  width: 100vw;
-  height: 50px;
-  border-bottom: solid 1px rgba(0, 0, 0, 0.5);
-  position: relative;
-  nav {
-    padding: 10px;
-    display: flex;
-    gap: 20px;
-    align-items: center;
-    justify-content: center;
-  }
-`;
+import { IHeaderProps } from "../../types/common/header";
+import HeaderView from "./view/HeaderView";
 
 export default function Header() {
   const [isLoggedIn, setIsLoggedIn] = useRecoilState(isLogged);
@@ -28,25 +13,11 @@ export default function Header() {
     setId(null);
     localStorage.removeItem("token");
   };
-  return (
-    <Wrapper>
-      {noticeMsg && <Notice />}
-      <nav>
-        <p>
-          <Link to="/">home</Link>
-        </p>
-        {!isLoggedIn ? (
-          <p>
-            <Link to="auth">login</Link>
-          </p>
-        ) : (
-          <p>
-            <Link to="/" onClick={onLogout}>
-              logout
-            </Link>
-          </p>
-        )}
-      </nav>
-    </Wrapper>
-  );
+
+  const headerProps: IHeaderProps = {
+    onLogout,
+    isLoggedIn,
+    noticeMsg,
+  };
+  return <HeaderView {...headerProps} />;
 }
